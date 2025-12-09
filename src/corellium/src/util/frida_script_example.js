4c468f71161467b962f6879f0dfa671a8e88ac50
davidcbacker <20198171+davidcbacker@users.noreply.github.com>
2025-12-09 11:28:22 -0500

    Update frida_script_example.js

--- src/corellium/src/util/frida_script_example.js
+++ src/corellium/src/util/frida_script_example.js
@@ -1,7 +1,6 @@
 console.log('------------------------------------------------');
 console.log('------------------------------------------------');
 console.log('---                                          ---');
-console.log('---   Android Corellium Cafe sample script   ---');
 console.log('---   Demo of a Frida script running in CI   ---');
 console.log('---                                          ---');
 console.log('------------------------------------------------');
